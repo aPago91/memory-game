af273117f544bf81516c41c04c0447c9686dee96
aPago91 <apagowski1991@gmail.com>
2018-08-30 17:52:40 +0200

    dodanie js zmienne

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const memory = {
+    tileCount : 16, 
+    tileOnRow : 4, 
+    divBoard : null, 
+    divScore : null, 
+    tiles : [], 
+    tilesChecked : [], 
+    moveCount : 0, 
+    tilesImg : [ 
+        '.../image/obrazek1.jpg',
+        '.../image/obrazek2.jpg',
+        '.../image/obrazek3.jpg',
+        '.../image/obrazek4.jpg',
+        '.../image/obrazek5.jpg',
+        '.../image/obrazek6.jpg',
+        '.../image/obrazek7.jpg',
+        '.../image/obrazek8.jpg',
+        '.../image/obrazek9.jpg',
+        '.../image/obrazek10.jpg'
+    ]
+}
