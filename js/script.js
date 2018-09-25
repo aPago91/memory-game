6f87fd83268d156ded942731827693305eefbaa2
aPago91 <apagowski1991@gmail.com>
2018-09-25 18:18:59 +0200

    Zmiana nazewnictwa z pl na eng

--- js/script.js
+++ js/script.js
@@ -3,83 +3,83 @@ var cards = ["Sylvanas.png", "Thrall.png", "Thrall.png", "Greymane.png", "Sylvan
 var scards = shuffle(cards);
 
 function shuffle(arr) {
-    var i,
-        j,
-        temp;
-    for (i = arr.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    return arr;    
+	var i,
+			j,
+			temp;
+	for (i = arr.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
 };
 
 for (let i = 0; i < 12; i++)
 {
-   document.getElementById('k'+i).addEventListener("click", function(){odslonietaKarta(i);});
+	document.getElementById('k'+i).addEventListener("click", function(){revealCard(i);});
 }
           
-var jednaWidoczna = false;
-var wlaczLicznik = 0;
-var widoczny_nr;
-var blokada = false;
-var pozostaleParty = 6;
+var oneVisible = false;
+var turnCounter = 0;
+var visible_nr;
+var block = false;
+var otherPair = 6;
 
-function odslonietaKarta(nr){
- var opacityValue = $('#k'+nr).css('opacity');
+function revealCard(nr){
+	var opacityValue = $('#k'+nr).css('opacity');
  
- if(opacityValue != 0 && blokada == false){
-  blokada = true;
+	if(opacityValue != 0 && block == false){
+		block = true;
   
-  var obraz = "url(image/" + cards[nr] + ")";
+		var img = "url(image/" + cards[nr] + ")";
  
-  $('#k'+nr).css('background-image', obraz);
-  $('#k'+nr).addClass('cardA');
-  $('#k'+nr).removeClass('card');
+		$('#k'+nr).css('background-image', img);
+		$('#k'+nr).addClass('cardA');
+		$('#k'+nr).removeClass('card');
  
-  if(jednaWidoczna == false){
+		if(oneVisible == false){
    
-   jednaWidoczna = true;
-   widoczny_nr = nr;
-   blokada = false;
-  }
-  else{
-   if(cards [widoczny_nr] == cards[nr]){
-    setTimeout(function() {schowaj2Karty(nr, widoczny_nr)}, 750);
-   }
-   else{
-    setTimeout(function() {odzyskaj2Karty(nr, widoczny_nr)}, 1000);
-   }
+			oneVisible = true;
+			visible_nr = nr;
+			block = false;
+		}
+		else{
+			if(cards [visible_nr] == cards[nr]){
+				setTimeout(function() {hide2Card(nr, visible_nr)}, 750);
+			}
+			else{
+				setTimeout(function() {get2Cards(nr, visible_nr)}, 1000);
+			}
    
-   wlaczLicznik++;
-   $('.score').html('Liczba ruchów: ' + wlaczLicznik);
-   jednaWidoczna = false;
-  }
- }
+			turnCounter++;
+			$('.score').html('Liczba ruchów: ' + turnCounter);
+			oneVisible = false;
+		}
+	}
 }
 
-function schowaj2Karty(nr1, nr2){
- $('#k' + nr1).css('opacity', '0');
- $('#k' + nr2).css('opacity', '0');
+function hide2Card(nr1, nr2){
+	$('#k' + nr1).css('opacity', '0');
+	$('#k' + nr2).css('opacity', '0');
  
- pozostaleParty--;
+	otherPair--;
  
- if(pozostaleParty == 0){
-  $('.board').html('<h1>Wygrałeś! Po '+wlaczLicznik+' ruchach.</h1></br><button onclick="location.reload();">Zrestartuj grę</button>');
- }
+	if(otherPair == 0){
+		$('.board').html('<h1>Wygrałeś! Po '+turnCounter+' ruchach.</h1></br><button onclick="location.reload();">Zrestartuj grę</button>');
+	}
  
- blokada = false;
+	block = false;
 }
 
-function odzyskaj2Karty(nr1, nr2){
- $('#k'+nr1).css('background-image', 'url(image/Zakryta.png)');
- $('#k'+nr1).addClass('card');
- $('#k'+nr1).removeClass('cardA');
+function get2Cards(nr1, nr2){
+	$('#k'+nr1).css('background-image', 'url(image/Zakryta.png)');
+	$('#k'+nr1).addClass('card');
+	$('#k'+nr1).removeClass('cardA');
  
- $('#k'+nr2).css('background-image', 'url(image/Zakryta.png)');
- $('#k'+nr2).addClass('card');
- $('#k'+nr2).removeClass('cardA');
+	$('#k'+nr2).css('background-image', 'url(image/Zakryta.png)');
+	$('#k'+nr2).addClass('card');
+	$('#k'+nr2).removeClass('cardA');
  
- blokada = false;
+	block = false;
 }
